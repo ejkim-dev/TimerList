dd1524f90b3c22fa4aa59df7af8e3d085395ed46
Eunjin Kim <biz.ejkim@gmail.com>
2025-02-01 16:06:56 +0900

    add auto delete adapter

---
+++ app/src/main/java/com/example/timerlist/adapter/AutoDeleteAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.timerlist.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.timerlist.databinding.ItemAutoDeleteBinding
+import com.example.timerlist.databinding.ItemNameBinding
+import item.AutoDelete
+
+class AutoDeleteAdapter : ListAdapter<AutoDelete, RecyclerView.ViewHolder>(AutoDeleteDiffUtil) {
+
+    private inner class DeleteIconWithNameViewHolder(binding: ItemNameBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val name = binding.textName
+    }
+
+    private inner class AutoDeleteViewHolder(binding: ItemAutoDeleteBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val name = binding.textDeleteName
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+
+        return if (viewType == 1) {
+            val binding = ItemAutoDeleteBinding.inflate(inflater, parent, false)
+            AutoDeleteViewHolder(binding)
+        } else {
+            val binding = ItemNameBinding.inflate(inflater, parent, false)
+            DeleteIconWithNameViewHolder(binding)
+        }
+    }
+
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val currentItem = getItem(position)
+
+        when (holder) {
+            is DeleteIconWithNameViewHolder -> {
+                holder.name.text = currentItem.name
+            }
+            is AutoDeleteViewHolder -> {
+                holder.name.text = currentItem.name
+            }
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (getItem(position).isRunning) 1 else 0
+    }
+}
